0402ada6e17b7a615d290bb24554b1d0806d413f
Dark Web <diovincalatero150@gmail.com>
2025-01-06 14:34:52 +0800

    commit

--- admin/user_student.php
+++ admin/user_student.php
@@ -52,6 +52,7 @@ include('./includes/sidebar.php');
                               <table id="example" class="display nowrap" style="width:100%">
                                         <thead>
                                              <tr>
+                                                  <th class="hidden"><center>Email</center></th>
                                                   <th><center>Full Name</center></th>
                                                   <th><center>Student Profile</center></th>
                                                   <th><center>Student No</center></th>
@@ -70,6 +71,7 @@ include('./includes/sidebar.php');
                                                   foreach($query_run as $user) {
                                                        ?>
                                                        <tr>
+                                                            <td><center><?=$user['email'];?></center></td>
                                                             <td style="text-transform:capitalize;"><center><?=$user['lastname'].',  '.$user['firstname'].' '.$user['middlename'];?></center></td>
                                                             <td>
                                                                  <center>
