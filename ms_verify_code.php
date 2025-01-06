3791c4e3da764bdf569ec00367fd399a6af7bc17
Dark Web <diovincalatero150@gmail.com>
2025-01-06 14:13:55 +0800

    commit

--- ms_verify_code.php
+++ ms_verify_code.php
@@ -86,12 +86,12 @@ if (isset($_POST['registration_link'])) {
             $mail->isSMTP();
             $mail->Host       = 'smtp.gmail.com';
             $mail->SMTPAuth   = true;
-            $mail->Username   = 'mcclearningresourcecenter2.0@gmail.com'; // Use environment variable
-            $mail->Password   = 'mbuq bvbh wtst tnsr'; // Use environment variable
+            $mail->Username   = 'mcclearningresourcecenterv2.0@gmail.com'; // Use environment variable
+            $mail->Password   = 'dowu dtzu gqve wrjq'; // Use environment variable
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
             $mail->Port       = 587;
 
-            $mail->setFrom('mcclearningresourcecenter2.0@gmail.com', 'MCC Learning Resource Center');
+            $mail->setFrom('mcclearningresourcecenterv2.0@gmail.com', 'MCC Learning Resource Center');
             $mail->addAddress($email);
 
             $mail->isHTML(true);
