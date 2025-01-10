45c119cef0cb7b55ecc707efca320202a88893f1
Dark Web <diovincalatero150@gmail.com>
2025-01-10 12:14:32 +0800

    commit

--- admin/ms_account.php
+++ admin/ms_account.php
@@ -5,125 +5,117 @@ include('./includes/sidebar.php');
 ?>
 
 <main id="main" class="main">
-     <div class="pagetitle" data-aos="fade-down">
-          <h1>MS 365 Account</h1>
-          <nav>
-               <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href=".">Home</a></li>
-                    <li class="breadcrumb-item active">MS 365 Account</li>
-               </ol>
-          </nav>
-     </div>
-     <section class="section dashboard">
-          <div class="row">
-               <div class="col-lg-12">
-                    <div class="row">
-                         <div data-aos="fade-down" class="col-12">
-                              <div class="card recent-sales overflow-auto border-3 border-top border-info">
-                                   <div class="card-body">
-                                        <div class="row d-flex justify-content-between align-items-center mt-4">
-                                             <div class="text-start">
-                                                  <form action="import.php" method="post" enctype="multipart/form-data">
-                                                       <input type="file" name="file" required>
-                                                       <button type="submit" name="save_excel_data" class="btn btn-primary">
-                                                             <b>Import</b>
-                                                       </button>
-                                                  </form>
-                                             </div>
-                                             <div class="text-end">
-                                                  <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#addAccountModal">
-                                                       <b>Add New Account</b>
-                                                  </button>
-                                             </div>
-                                        </div>
-                                        <br>
-                                        <div class="container">
-                                             <div class="row">
-                                                  <div class="col-12">
-                                                       <div class="data_table">
-                                                       <table id="example" class="display nowrap" style="width:100%">
-                                                                 <br>
-                                                                 <thead>
-                                                                      <tr>
-                                                                           <th class="d-none">MS ID</th>
-                                                                           <th>Firstname</th>
-                                                                           <th>Lastname</th>
-                                                                           <th>Email</th>
-                                                                           <th>Used</th>
-                                                                           <th>Action</th>
-                                                                      </tr>
-                                                                 </thead>
-                                                                 <tbody>
-                                                                      <?php
-                                                                      $query = "SELECT * FROM ms_account";
-                                                                      if ($stmt = $con->prepare($query)) {
-                                                                           $stmt->execute();
-                                                                           $result = $stmt->get_result();
-                                                                           while ($row = $result->fetch_assoc()) {
-                                                                                echo "<tr>
-                                                                                          <td class='d-none'>{$row['ms_id']}</td>
-                                                                                          <td>{$row['firstname']}</td>
-                                                                                          <td>{$row['lastname']}</td>
-                                                                                          <td>{$row['username']}</td>
-                                                                                          <td>{$row['used']}</td>
-                                                                                          <td>
-                                                                                          <button class='btn btn-warning editBtn' data-bs-toggle='modal' data-bs-target='#editAccountModal' 
-                                                                                                    data-msid='{$row['ms_id']}' data-used='{$row['used']}'>
-                                                                                               Edit
-                                                                                          </button>
-                                                                                          </td>
-                                                                                     </tr>";
-                                                                           }
-                                                                           $stmt->close();
-                                                                      } else {
-                                                                           echo "<tr><td colspan='6'>Error retrieving data</td></tr>";
-                                                                      }
-                                                                      ?>
-                                                                 </tbody>
-                                                            </table>
-                                                       </div>
-                                                  </div>
-                                             </div>
-                                        </div>
-                                   </div>
-                              </div>
-                         </div>
+    <div class="pagetitle" data-aos="fade-down">
+        <h1>MS 365 Account</h1>
+        <nav>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href=".">Home</a></li>
+                <li class="breadcrumb-item active">MS 365 Account</li>
+            </ol>
+        </nav>
+    </div>
+
+    <section class="section dashboard">
+        <div class="row">
+            <div class="col-12">
+                <div class="card recent-sales overflow-auto border-3 border-top border-info">
+                    <div class="card-body">
+                        <div class="row gap-3 gap-md-0 d-flex justify-content-between align-items-center mt-4">
+                            <div class="col-md-6 text-start">
+                                <form action="import.php" method="post" enctype="multipart/form-data">
+                                    <div class="input-group">
+                                        <input type="file" name="file" class="form-control" required>
+                                        <button type="submit" name="save_excel_data" class="btn btn-primary">
+                                            <b>Import</b>
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                            <div class="col-md-4 text-end">
+                                <button class="btn btn-success w-100" data-bs-toggle="modal" data-bs-target="#addAccountModal">
+                                    <b>Add New Account</b>
+                                </button>
+                            </div>
+                        </div>
+                        <br>
+                        <div class="table-responsive">
+                            <table id="example" class="table table-striped table-bordered nowrap" style="width:100%">
+                                <thead>
+                                    <tr>
+                                        <th class="d-none">MS ID</th>
+                                        <th>Firstname</th>
+                                        <th>Lastname</th>
+                                        <th>Email</th>
+                                        <th>Used</th>
+                                        <th>Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $query = "SELECT * FROM ms_account";
+                                    if ($stmt = $con->prepare($query)) {
+                                        $stmt->execute();
+                                        $result = $stmt->get_result();
+                                        while ($row = $result->fetch_assoc()) {
+                                            echo "<tr>
+                                                    <td class='d-none'>{$row['ms_id']}</td>
+                                                    <td>{$row['firstname']}</td>
+                                                    <td>{$row['lastname']}</td>
+                                                    <td>{$row['username']}</td>
+                                                    <td>{$row['used']}</td>
+                                                    <td>
+                                                        <button class='btn btn-warning editBtn' data-bs-toggle='modal' data-bs-target='#editAccountModal' 
+                                                                data-msid='{$row['ms_id']}' data-used='{$row['used']}'>
+                                                            Edit
+                                                        </button>
+                                                    </td>
+                                                </tr>";
+                                        }
+                                        $stmt->close();
+                                    } else {
+                                        echo "<tr><td colspan='6'>Error retrieving data</td></tr>";
+                                    }
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
-               </div>
-          </div>
-     </section>
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
 
 <!-- Add Account Modal -->
 <div class="modal fade" id="addAccountModal" tabindex="-1" aria-labelledby="addAccountModalLabel" aria-hidden="true">
-     <div class="modal-dialog">
-          <div class="modal-content">
-               <div class="modal-header">
-                    <h5 class="modal-title" id="addAccountModalLabel">Add New MS 365 Account</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-               </div>
-               <form action="add_account.php" method="post">
-                    <div class="modal-body">
-                         <div class="mb-3">
-                              <label for="firstname" class="form-label">Firstname</label>
-                              <input type="text" class="form-control" id="firstname" name="firstname" required>
-                         </div>
-                         <div class="mb-3">
-                              <label for="lastname" class="form-label">Lastname</label>
-                              <input type="text" class="form-control" id="lastname" name="lastname" required>
-                         </div>
-                         <div class="mb-3">
-                              <label for="username" class="form-label">Email</label>
-                              <input type="email" class="form-control" id="username" name="username" required>
-                         </div>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="addAccountModalLabel">Add New MS 365 Account</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action="add_account.php" method="post">
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="firstname" class="form-label">Firstname</label>
+                        <input type="text" class="form-control" id="firstname" name="firstname" required>
                     </div>
-                    <div class="modal-footer">
-                         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                         <button type="submit" name="add_account" class="btn btn-primary">Save</button>
+                    <div class="mb-3">
+                        <label for="lastname" class="form-label">Lastname</label>
+                        <input type="text" class="form-control" id="lastname" name="lastname" required>
                     </div>
-               </form>
-          </div>
-     </div>
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="username" name="username" required>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" name="add_account" class="btn btn-primary">Save</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
 
 <!-- Edit Account Modal -->
@@ -139,7 +131,7 @@ include('./includes/sidebar.php');
                     <div class="mb-3">
                         <label for="used" class="form-label">Used</label>
                         <input type="text" class="form-control" id="used" name="used" required>
-                        <input type="hidden" id="ms_id" name="ms_id"> <!-- Hidden field to pass ms_id -->
+                        <input type="hidden" id="ms_id" name="ms_id">
                     </div>
                 </div>
                 <div class="modal-footer">
@@ -150,7 +142,6 @@ include('./includes/sidebar.php');
         </div>
     </div>
 </div>
-
 
 <?php 
 include('./includes/footer.php');
@@ -171,15 +162,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Event listener for the Edit button
     const editButtons = document.querySelectorAll('.editBtn');
-        editButtons.forEach(button => {
-            button.addEventListener('click', function() {
-                const msid = this.getAttribute('data-msid');
-                const used = this.getAttribute('data-used');
-                
-                // Populate the modal with the current data
-                document.getElementById('ms_id').value = msid;
-                document.getElementById('used').value = used;
-            });
+    editButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const msid = this.getAttribute('data-msid');
+            const used = this.getAttribute('data-used');
+            document.getElementById('ms_id').value = msid;
+            document.getElementById('used').value = used;
         });
+    });
 });
 </script>
