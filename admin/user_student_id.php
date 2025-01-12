39f55c2c11897fe76a86cb27bed09ee89db1ab4a
Dark Web <diovincalatero150@gmail.com>
2025-01-12 21:28:11 +0800

    a

--- admin/user_student_id.php
+++ admin/user_student_id.php
@@ -147,7 +147,7 @@ if ($row) {
     height: 100%;
     background: url('images/bg-front.png');
     background-repeat: repeat-x;
-    background-size: 240px 330px;
+    background-size: 220px 330px;
     opacity: 0.2;
     z-index: -1;
     text-align: center;
