497af4d4efba4c7f22a64272c1692675452318a9
Dark Web <diovincalatero150@gmail.com>
2025-02-03 13:59:31 +0800

    commit

--- admin/book_add.php
+++ admin/book_add.php
@@ -136,8 +136,7 @@ include('./includes/sidebar.php');
                                         <!-- Hidden input to hold the existing image filename -->
                                         <input type="hidden" name="existing_image" id="existing_image">
                                     </div>
-                                </div>
-                                <div class="col-12 col-md-4">
+                                    <br>
                                     <div class="mb-2 input-group-sm">
                                         <label for="page">Page</label>
                                         <input type="text" id="page" name="page" class="form-control">
