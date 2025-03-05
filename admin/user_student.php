ed8f3d2dd91f280f48f9f184c0c3b5f4ef58e031
Dark Web <diovincalatero150@gmail.com>
2025-03-05 12:58:39 +0800

    commit

--- admin/user_student.php
+++ admin/user_student.php
@@ -217,31 +217,6 @@ function confirmDelete(userId) {
         });
 }
 
-function loadStudentData(userId) {
-    fetch('user_student_code.php?id=' + userId)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            // Populate modal fields with sanitized data
-            document.getElementById('editStudentId').value = sanitizeInput(data.user_id);
-            document.getElementById('editLName').value = sanitizeInput(data.lastname);
-            document.getElementById('editFName').value = sanitizeInput(data.firstname);
-            document.getElementById('editMName').value = sanitizeInput(data.middlename);
-            document.getElementById('editStuID').value = sanitizeInput(data.student_id_no);
-
-            // Show modal
-            var myModal = new bootstrap.Modal(document.getElementById('editStudentModal'));
-            myModal.show();
-        })
-        .catch(error => {
-            console.error('Error fetching student data:', error);
-        });
-}
-
 document.getElementById('editLName').addEventListener('input', function () {
      var editLName = this.value.trim(); // Remove any leading or trailing spaces
           
