8d764e6cfa2f498d5e5999071e5c0be08f7a99f4
Dark Web <diovincalatero031402@gmail.com>
2025-05-05 13:50:47 +0800

    c

--- admin/ms_account_code.php
+++ admin/ms_account_code.php
@@ -40,8 +40,8 @@ if (isset($_GET['id'])) {
     $result = mysqli_query($con, $sql);
 
     if ($result && mysqli_num_rows($result) > 0) {
-        $used = mysqli_fetch_assoc($result);
-        echo json_encode($used);
+        $row = mysqli_fetch_assoc($result);
+        echo json_encode($row);
     } else {
         $_SESSION['status'] = "Account not found";
         $_SESSION['status_code'] = "error";
