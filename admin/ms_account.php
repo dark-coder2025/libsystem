a17b863c2efc584d84d90a019ec8fd0dfdb82c64
Dark Web <diovincalatero031402@gmail.com>
2025-05-05 13:55:24 +0800

    c

--- admin/ms_account.php
+++ admin/ms_account.php
@@ -85,7 +85,7 @@ include('./includes/sidebar.php');
     </section>
 </main>
 
-<!-- Add Account Modal
+Add Account Modal
 <div class="modal fade" id="addAccountModal" tabindex="-1" aria-labelledby="addAccountModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
@@ -115,9 +115,9 @@ include('./includes/sidebar.php');
             </form>
         </div>
     </div>
-</div> -->
+</div>
 
-<!-- Edit Account Modal -->
+<!-- Edit Account Modal
 <div class="modal fade" id="editAccountModal" tabindex="-1" aria-labelledby="editAccountModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
@@ -140,7 +140,7 @@ include('./includes/sidebar.php');
       </div>
     </div>
   </div>
-</div>
+</div> -->
 
 <?php 
 include('./includes/footer.php');
