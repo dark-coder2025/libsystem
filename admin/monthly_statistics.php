6caedb1794c46041d44b4e943ac4fa7626359c7e
Dark Coder <coder.dark2025@gmail.com>
2025-05-15 10:48:46 +0800

    c

--- admin/monthly_statistics.php
+++ admin/monthly_statistics.php
@@ -20,8 +20,6 @@ $startDate = $month . '-01';
 $endDate = date('Y-m-t', strtotime($startDate));
 
 // Prepare and execute the query
-$courses = [];
-$total_student_course = [];
 $query = "SELECT course, COUNT(course) as count FROM user_log WHERE date_log BETWEEN '$startDate' AND '$endDate' GROUP BY course";
 $query_run = mysqli_query($con, $query);
 
