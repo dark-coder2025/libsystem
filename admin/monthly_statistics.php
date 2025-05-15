55af185b2ca4bd3704b9dff89a47df3e53fb1f4c
Dark Coder <coder.dark2025@gmail.com>
2025-05-15 15:12:14 +0800

    c

--- admin/monthly_statistics.php
+++ admin/monthly_statistics.php
@@ -136,7 +136,7 @@ if ($query_run && mysqli_num_rows($query_run) > 0) {
     #printArea {
         position: absolute;
         left: 0;
-        top: 100%;
+        top: 50%;
         width: 100%;
     }
 
