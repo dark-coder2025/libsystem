a93e1ae3df9e1abdf8eb57fd5b75a7b7c5662400
Dark Coder <coder.dark2025@gmail.com>
2025-05-15 10:16:42 +0800

    c

--- admin/index.php
+++ admin/index.php
@@ -307,7 +307,6 @@ $total_volumes = $row_volumes['total_volumes'];
                                                                       'rgba(201, 203, 207, 0.2)'
                                                                  ],
                                                                  borderColor: [
-                                                                      'rgb(255, 99, 132)',
                                                                       'rgb(255, 159, 64)',
                                                                       'rgb(255, 205, 86)',
                                                                       'rgb(75, 192, 192)',
