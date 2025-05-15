937b300a9aeaa0b67fb1c693a22983ed2b36088f
Dark Coder <coder.dark2025@gmail.com>
2025-05-15 15:34:07 +0800

    c

--- admin/attendance.php
+++ admin/attendance.php
@@ -3,45 +3,14 @@ include('authentication.php');
 include('includes/header.php'); 
 include('./includes/sidebar.php'); 
 
-// // Bulk delete handler with SweetAlert
-// if (isset($_POST['bulk_delete']) && isset($_POST['delete_ids'])) {
-//     $ids = $_POST['delete_ids'];
-//     $ids_str = implode(",", array_map('intval', $ids)); // sanitize IDs
-//     $delete_query = "DELETE FROM user_log WHERE user_log_id IN ($ids_str)";
-    
-//     if (mysqli_query($con, $delete_query)) {
-//         echo "
-//         <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
-//         <script>
-//         document.addEventListener('DOMContentLoaded', function() {
-//             Swal.fire({
-//                 icon: 'success',
-//                 title: 'Deleted!',
-//                 text: 'Selected logs have been deleted.',
-//                 confirmButtonColor: '#3085d6',
-//                 confirmButtonText: 'OK'
-//             }).then(() => {
-//                 window.location.href = window.location.href;
-//             });
-//         });
-//         </script>";
-//     } else {
-//         $error = mysqli_error($con);
-//         echo "
-//         <script src='https://cdn.jsdelivr.net/npm/sweetalert2@11'></script>
-//         <script>
-//         document.addEventListener('DOMContentLoaded', function() {
-//             Swal.fire({
-//                 icon: 'error',
-//                 title: 'Error!',
-//                 text: 'Could not delete logs: $error',
-//                 confirmButtonColor: '#d33',
-//                 confirmButtonText: 'Close'
-//             });
-//         });
-//         </script>";
-//     }
-// }
+// if (isset($_POST['delete_all'])) {
+//      $delete_query = "DELETE FROM user_log";
+//      if (mysqli_query($con, $delete_query)) {
+//          echo "<script>alert('All user logs have been deleted successfully.');</script>";
+//      } else {
+//          echo "<script>alert('Error deleting user logs: " . mysqli_error($con) . "');</script>";
+//      }
+//  }
 ?>
 
 <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css">
@@ -49,7 +18,9 @@ include('./includes/sidebar.php');
 
 <main id="main" class="main">
      <div class="pagetitle" data-aos="fade-down">
+
           <h1>Attendance</h1>
+
           <nav>
                <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href=".">Home</a></li>
@@ -62,93 +33,119 @@ include('./includes/sidebar.php');
           <div class="row">
                <div class="col-lg-12">
                     <div class="row">
+                         <div class="row">
                          <div data-aos="fade-down" class="col-12">
-                              <div class="card recent-sales overflow-auto border-3 border-top border-info">
+                              <div class="card recent-sales overflow-auto  border-3 border-top border-info">
+
                                    <div class="card-body">
                                         <div class="row d-flex justify-content-end align-items-center mt-2">
-                                             <form action="" method="POST" class="col-12 col-md-5 d-flex">
+                                             <form action="" method="POST" class="col-12 col-md-5 d-flex ">
                                                   <?php date_default_timezone_set('Asia/Manila'); ?>
                                                   <div class="form-group form-group-sm">
-                                                       <label><small>From Date</small></label>
-                                                       <input type="date" name="from_date" class="form-control form-control-sm">
+                                                       <label for=""> <small>From Date</small></label>
+                                                       <input type="date" name="from_date" id="disable_date"
+                                                            class="form-control form-control-sm"></input>
                                                   </div>
+
                                                   <div class="form-group form-group-sm mx-2">
-                                                       <label><small>To Date</small></label>
-                                                       <input type="date" name="to_date" class="form-control form-control-sm">
+                                                       <label for=""> <small>To Date</small></label>
+                                                       <input type="date" name="to_date" id="disable_date2"
+                                                            class="form-control form-control-sm"></input>
                                                   </div>
                                                   <div class="form-group form-group-sm">
-                                                       <label><small>Click to Filter</small></label>
-                                                       <button type="submit" name="filter_attendance" class="btn text-white fw-semibold btn-info btn-sm d-block">Filter</button>
+                                                       <label for=""> <small>Click to Filter</small></label>
+                                                       <button type="submit" name="filter_attendance"
+                                                            class="btn text-white fw-semibold btn-info btn-sm d-block">Filter</button>
                                                   </div>
                                              </form>
+
+                                             <!-- <form action="" method="POST" class="col-12 col-md-3 d-flex justify-content-center">
+                                                            <button type="submit" name="delete_all" class="btn btn-danger btn-sm">Delete All Logs</button>
+                                                       </form> -->
+
                                         </div>
 
-                                        <form method="POST" id="bulkDeleteForm">
-                                        <div class="container mt-3">
+                                        <div class="container">
                                              <div class="row">
                                                   <div class="col-12">
+
                                                        <div class="data_table">
-                                                            <table id="example3" class="display" style="width:100%">
+                                                       <table id="example3" class="display" style="width:100%">
                                                                  <thead>
                                                                       <tr>
-                                                                           <!-- <th><input type="checkbox" id="select-all"></th> -->
-                                                                           <th>Date</th>
-                                                                           <th>Time In</th>
-                                                                           <th>Full Name</th>
-                                                                           <th>Program</th>
-                                                                           <th>Time Out</th>
+                                                                      <th>Date</th>
+                                                                                     <th>Time in</th>
+                                                                                     <th>Full Name</th>
+                                                                                     <th>Program</th>
+                                                                                     <th>Time out</th>
                                                                       </tr>
                                                                  </thead>
                                                                  <tbody>
                                                                       <?php
-                                                                      if (isset($_POST['from_date']) && isset($_POST['to_date'])) {
-                                                                           $from_date = $_POST['from_date'];
-                                                                           $to_date = $_POST['to_date'];
-                                                                           $query = "SELECT * FROM user_log WHERE date_log BETWEEN '$from_date' AND '$to_date' ORDER BY date_log DESC, time_log DESC";
-                                                                           $query_run = mysqli_query($con, $query);
-                                                                           if (mysqli_num_rows($query_run) > 0) {
-                                                                                foreach ($query_run as $row) {
-                                                                      ?>
-                                                                                     <tr>
-                                                                                          <!-- <td><input type="checkbox" name="delete_ids[]" value="<?= $row['user_log_id']; ?>"></td> -->
-                                                                                          <td><?= date("M d, Y", strtotime($row['date_log'])); ?></td>
-                                                                                          <td><?= date("h:i a", strtotime($row['time_log'])); ?></td>
-                                                                                          <td><?= $row['firstname'] . ' ' . $row['middlename'] . ' ' . $row['lastname']; ?></td>
-                                                                                          <td><?= $row['year_level'] . ' - ' . $row['course']; ?></td>
-                                                                                          <td><?= date("h:i a", strtotime($row['time_out'])); ?></td>
-                                                                                     </tr>
-                                                                      <?php
-                                                                                }
-                                                                           }
-                                                                      } else {
-                                                                           $result = mysqli_query($con, "SELECT * FROM user_log ORDER BY date_log DESC, time_log DESC");
-                                                                           while ($row = mysqli_fetch_array($result)) {
-                                                                      ?>
-                                                                                     <tr>
-                                                                                          <!-- <td><input type="checkbox" name="delete_ids[]" value="<?= $row['user_log_id']; ?>"></td> -->
-                                                                                          <td><?= date("M d, Y", strtotime($row['date_log'])); ?></td>
-                                                                                          <td><?= date("h:i a", strtotime($row['time_log'])); ?></td>
-                                                                                          <td><?= $row['firstname'] . ' ' . $row['middlename'] . ' ' . $row['lastname']; ?></td>
-                                                                                          <td><?= $row['year_level'] . ' - ' . $row['course']; ?></td>
-                                                                                          <td><?= date("h:i a", strtotime($row['time_out'])); ?></td>
-                                                                                     </tr>
-                                                                      <?php
-                                                                           }
-                                                                      }
-                                                                      ?>
+                                 
+                                                       
+                                 if(isset($_POST['from_date']) && isset($_POST['to_date']))
+                                 {
+                                      $from_date = $_POST['from_date'];
+                                      $to_date = $_POST['to_date'];
+
+                                      $query = "SELECT * FROM user_log WHERE date_log BETWEEN '$from_date' AND '$to_date' ORDER BY date_log DESC, time_log DESC";
+                                      $query_run = mysqli_query($con, $query);
+
+                                      if(mysqli_num_rows($query_run) > 0 )
+                                      {
+                                           foreach($query_run as $row)
+                                           {
+                                 ?>
+                                                          <tr>
+                                                               <?php date_default_timezone_set('Asia/Manila'); ?>
+                                                               
+                                                            <td><?= date("M d, Y", strtotime($row['date_log'])); ?></td>
+                                                            <td><?= date("h:i a", strtotime($row['time_log'])); ?></td>
+                                                            <td><?= $row['firstname'].' '.$row['middlename'].' '.$row['lastname']; ?></td>
+                                                            <td><?=$row['year_level'].' - '.$row['course']; ?></td>
+                                                            <td><?= date("h:i a", strtotime($row['time_out'])); ?></td>
+                                                          </tr>
+                                                          <?php      }
+                            }
+                            
+                       }
+                       else
+                       {
+                       
+                            $result= mysqli_query($con,"SELECT * FROM user_log ORDER BY date_log DESC, time_log DESC");
+                            while ($row= mysqli_fetch_array ($result) ){
+                           
+                                                  ?>
+                                                                      <tr>
+                                                                                     <?php date_default_timezone_set('Asia/Manila'); ?>
+                                                                                     <td><?= date("M d, Y", strtotime($row['date_log'])); ?>
+                                                                                     </td>
+                                                                                     <td><?= date("h:i a", strtotime($row['time_log'])); ?>
+                                                                                     </td>
+                                                                                     <td><?= $row['firstname'].' '.$row['middlename'].' '.$row['lastname']; ?>
+                                                                                     </td>
+                                                                                     <td><?=$row['year_level'].' - '.$row['course']; ?></td>
+                                                                                     <td><?= date("h:i a", strtotime($row['time_out'])); ?></td>
+                                                                                </tr>
+                                                                      <?php } 
+                                                       }
+                                                 
+                                                       ?>
+
                                                                  </tbody>
                                                             </table>
                                                        </div>
-                                                       <!-- <button type="submit" name="bulk_delete" class="btn btn-danger mt-3">Delete Selected</button> -->
                                                   </div>
                                              </div>
                                         </div>
-                                        </form>
                                    </div>
                               </div>
                          </div>
+
                     </div>
                </div>
+
           </div>
      </section>
 </main>
@@ -171,12 +168,11 @@ include('./includes/sidebar.php');
 <script src="https://cdn.datatables.net/buttons/3.1.2/js/buttons.html5.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <script>
     const initDataTable = (selector) => {
         new DataTable(selector, {
-            order: [[1, 'asc']],
+            order: [[0, 1, 'asc']],
             layout: {
                 topStart: {
                     buttons: [
@@ -197,7 +193,7 @@ include('./includes/sidebar.php');
             language: {
                 buttons: {
                     copyTitle: 'Added to clipboard',
-                    copyKeys: 'Press <i>ctrl</i> or <i>⌘</i> + <i>C</i> to copy the table data. <br><br>To cancel, press Esc.',
+                    copyKeys: 'Press <i>ctrl</i> or <i>\u2318</i> + <i>C</i> to copy the table data to your clipboard. <br><br>To cancel, click this message or press Esc.',
                     copySuccess: {
                         _: '%d rows copied',
                         1: '1 row copied'
@@ -207,10 +203,12 @@ include('./includes/sidebar.php');
         });
     };
 
+    // Function to close the modal
     function closeModal() {
         document.getElementById('monthModal').style.display = 'none';
     }
 
+    // Function to filter by month and open new tab
     function filterByMonth() {
         const month = document.getElementById('selectedMonth').value;
         if (month) {
@@ -218,20 +216,19 @@ include('./includes/sidebar.php');
             window.open(url, '_blank');
             closeModal();
         } else {
-            Swal.fire('Please select a month.');
+            alert('Please select a month.');
         }
     }
 
-    document.getElementById('select-all').addEventListener('change', function () {
-        const checkboxes = document.querySelectorAll('input[name="delete_ids[]"]');
-        checkboxes.forEach(cb => cb.checked = this.checked);
-    });
-
     initDataTable('#example3');
 </script>
+
+
 
 <?php 
 include('./includes/footer.php');
 include('./includes/script.php');
 include('../message.php');
+
+    
 ?>
