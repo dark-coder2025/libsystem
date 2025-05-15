5e479b4d61c490a10fd89c57039ed7cd57d334cf
Dark Coder <coder.dark2025@gmail.com>
2025-05-15 14:26:25 +0800

    c

--- admin/user_faculty_block.php
+++ admin/user_faculty_block.php
@@ -26,6 +26,9 @@ include('./includes/sidebar.php');
           <div class="row">
                <div class="col-lg-12">
                     <div class="card">
+                         <div class="card-header d-flex justify-content-between align-items-center">
+                              <a href="users_block.php" class="btn btn-primary position-relative">Back</a>
+                         </div>
                          <div class="card-body">
                               <div class="table-responsive mt-3">
                               <table id="example" class="display nowrap" style="width:100%">
