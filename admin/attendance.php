e57add4b93680c62f97073f2e514092a13763299
Dark Coder <coder.dark2025@gmail.com>
2025-05-15 15:33:29 +0800

    c

--- admin/attendance.php
+++ admin/attendance.php
@@ -121,7 +121,7 @@ include('./includes/sidebar.php');
                                                                                 }
                                                                            }
                                                                       } else {
-                                                                           $result = mysqli_query($con, "SELECT * FROM user_log WHERE course = ''");
+                                                                           $result = mysqli_query($con, "SELECT * FROM user_log ORDER BY date_log DESC, time_log DESC");
                                                                            while ($row = mysqli_fetch_array($result)) {
                                                                       ?>
                                                                                      <tr>
