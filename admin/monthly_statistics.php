aa0d69f45414d2e4aaabbf45f36120c84fb79c6c
Dark Coder <coder.dark2025@gmail.com>
2025-05-15 15:50:17 +0800

    c

--- admin/monthly_statistics.php
+++ admin/monthly_statistics.php
@@ -70,6 +70,7 @@ if ($query_run && mysqli_num_rows($query_run) > 0) {
                                             new Chart(document.querySelector('#barChart'), {
                                                 type: 'bar',
                                                 data: {
+                                                    labels: <?= json_encode($labels) ?>,
                                                     datasets: [{
                                                         label: 'Program',
                                                         data: <?= json_encode($total_student_course) ?>,
