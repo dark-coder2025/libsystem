d6796a27b05991e1575721e6595c887f987e741f
Dark Coder <coder.dark2025@gmail.com>
2025-05-15 15:18:30 +0800

    c

--- admin/attendance.php
+++ admin/attendance.php
@@ -3,14 +3,16 @@ include('authentication.php');
 include('includes/header.php'); 
 include('./includes/sidebar.php'); 
 
-// if (isset($_POST['delete_all'])) {
-//      $delete_query = "DELETE FROM user_log";
-//      if (mysqli_query($con, $delete_query)) {
-//          echo "<script>alert('All user logs have been deleted successfully.');</script>";
-//      } else {
-//          echo "<script>alert('Error deleting user logs: " . mysqli_error($con) . "');</script>";
-//      }
-//  }
+// Handle individual delete
+if (isset($_POST['delete_log'])) {
+    $log_id = intval($_POST['delete_log_id']); // sanitize input
+    $delete_query = "DELETE FROM user_log WHERE user_log_id = '$log_id'";
+    if (mysqli_query($con, $delete_query)) {
+        echo "<script>alert('Log deleted successfully.'); window.location.href=window.location.href;</script>";
+    } else {
+        echo "<script>alert('Error deleting log: " . mysqli_error($con) . "');</script>";
+    }
+}
 ?>
 
 <link rel="stylesheet" href="https://cdn.datatables.net/2.1.8/css/dataTables.dataTables.css">
@@ -18,9 +20,7 @@ include('./includes/sidebar.php');
 
 <main id="main" class="main">
      <div class="pagetitle" data-aos="fade-down">
-
           <h1>Attendance</h1>
-
           <nav>
                <ol class="breadcrumb">
                     <li class="breadcrumb-item"><a href=".">Home</a></li>
@@ -33,106 +33,89 @@ include('./includes/sidebar.php');
           <div class="row">
                <div class="col-lg-12">
                     <div class="row">
-                         <div class="row">
                          <div data-aos="fade-down" class="col-12">
-                              <div class="card recent-sales overflow-auto  border-3 border-top border-info">
-
+                              <div class="card recent-sales overflow-auto border-3 border-top border-info">
                                    <div class="card-body">
                                         <div class="row d-flex justify-content-end align-items-center mt-2">
-                                             <form action="" method="POST" class="col-12 col-md-5 d-flex ">
+                                             <form action="" method="POST" class="col-12 col-md-5 d-flex">
                                                   <?php date_default_timezone_set('Asia/Manila'); ?>
                                                   <div class="form-group form-group-sm">
-                                                       <label for=""> <small>From Date</small></label>
-                                                       <input type="date" name="from_date" id="disable_date"
-                                                            class="form-control form-control-sm"></input>
+                                                       <label><small>From Date</small></label>
+                                                       <input type="date" name="from_date" class="form-control form-control-sm">
                                                   </div>
-
                                                   <div class="form-group form-group-sm mx-2">
-                                                       <label for=""> <small>To Date</small></label>
-                                                       <input type="date" name="to_date" id="disable_date2"
-                                                            class="form-control form-control-sm"></input>
+                                                       <label><small>To Date</small></label>
+                                                       <input type="date" name="to_date" class="form-control form-control-sm">
                                                   </div>
                                                   <div class="form-group form-group-sm">
-                                                       <label for=""> <small>Click to Filter</small></label>
-                                                       <button type="submit" name="filter_attendance"
-                                                            class="btn text-white fw-semibold btn-info btn-sm d-block">Filter</button>
+                                                       <label><small>Click to Filter</small></label>
+                                                       <button type="submit" name="filter_attendance" class="btn text-white fw-semibold btn-info btn-sm d-block">Filter</button>
                                                   </div>
                                              </form>
-
-                                             <!-- <form action="" method="POST" class="col-12 col-md-3 d-flex justify-content-center">
-                                                            <button type="submit" name="delete_all" class="btn btn-danger btn-sm">Delete All Logs</button>
-                                                       </form> -->
-
                                         </div>
 
-                                        <div class="container">
+                                        <div class="container mt-3">
                                              <div class="row">
                                                   <div class="col-12">
-
                                                        <div class="data_table">
-                                                       <table id="example3" class="display" style="width:100%">
+                                                            <table id="example3" class="display" style="width:100%">
                                                                  <thead>
                                                                       <tr>
-                                                                      <th>Date</th>
-                                                                                     <th>Time in</th>
-                                                                                     <th>Full Name</th>
-                                                                                     <th>Program</th>
-                                                                                     <th>Time out</th>
+                                                                           <th>Date</th>
+                                                                           <th>Time In</th>
+                                                                           <th>Full Name</th>
+                                                                           <th>Program</th>
+                                                                           <th>Time Out</th>
+                                                                           <th>Action</th>
                                                                       </tr>
                                                                  </thead>
                                                                  <tbody>
                                                                       <?php
-                                 
-                                                       
-                                 if(isset($_POST['from_date']) && isset($_POST['to_date']))
-                                 {
-                                      $from_date = $_POST['from_date'];
-                                      $to_date = $_POST['to_date'];
-
-                                      $query = "SELECT * FROM user_log WHERE date_log BETWEEN '$from_date' AND '$to_date' ORDER BY date_log DESC, time_log DESC";
-                                      $query_run = mysqli_query($con, $query);
-
-                                      if(mysqli_num_rows($query_run) > 0 )
-                                      {
-                                           foreach($query_run as $row)
-                                           {
-                                 ?>
-                                                          <tr>
-                                                               <?php date_default_timezone_set('Asia/Manila'); ?>
-                                                               
-                                                            <td><?= date("M d, Y", strtotime($row['date_log'])); ?></td>
-                                                            <td><?= date("h:i a", strtotime($row['time_log'])); ?></td>
-                                                            <td><?= $row['firstname'].' '.$row['middlename'].' '.$row['lastname']; ?></td>
-                                                            <td><?=$row['year_level'].' - '.$row['course']; ?></td>
-                                                            <td><?= date("h:i a", strtotime($row['time_out'])); ?></td>
-                                                          </tr>
-                                                          <?php      }
-                            }
-                            
-                       }
-                       else
-                       {
-                       
-                            $result= mysqli_query($con,"SELECT * FROM user_log ORDER BY date_log DESC, time_log DESC");
-                            while ($row= mysqli_fetch_array ($result) ){
-                           
-                                                  ?>
-                                                                      <tr>
-                                                                                     <?php date_default_timezone_set('Asia/Manila'); ?>
-                                                                                     <td><?= date("M d, Y", strtotime($row['date_log'])); ?>
-                                                                                     </td>
-                                                                                     <td><?= date("h:i a", strtotime($row['time_log'])); ?>
-                                                                                     </td>
-                                                                                     <td><?= $row['firstname'].' '.$row['middlename'].' '.$row['lastname']; ?>
-                                                                                     </td>
-                                                                                     <td><?=$row['year_level'].' - '.$row['course']; ?></td>
-                                                                                     <td><?= date("h:i a", strtotime($row['time_out'])); ?></td>
-                                                                                </tr>
-                                                                      <?php } 
-                                                       }
-                                                 
-                                                       ?>
-
+                                                                      if (isset($_POST['from_date']) && isset($_POST['to_date'])) {
+                                                                           $from_date = $_POST['from_date'];
+                                                                           $to_date = $_POST['to_date'];
+                                                                           $query = "SELECT * FROM user_log WHERE date_log BETWEEN '$from_date' AND '$to_date' ORDER BY date_log DESC, time_log DESC";
+                                                                           $query_run = mysqli_query($con, $query);
+                                                                           if (mysqli_num_rows($query_run) > 0) {
+                                                                                foreach ($query_run as $row) {
+                                                                      ?>
+                                                                                     <tr>
+                                                                                          <td><?= date("M d, Y", strtotime($row['date_log'])); ?></td>
+                                                                                          <td><?= date("h:i a", strtotime($row['time_log'])); ?></td>
+                                                                                          <td><?= $row['firstname'] . ' ' . $row['middlename'] . ' ' . $row['lastname']; ?></td>
+                                                                                          <td><?= $row['year_level'] . ' - ' . $row['course']; ?></td>
+                                                                                          <td><?= date("h:i a", strtotime($row['time_out'])); ?></td>
+                                                                                          <td>
+                                                                                               <form method="POST" action="" onsubmit="return confirm('Are you sure you want to delete this log?');">
+                                                                                                    <input type="hidden" name="delete_log_id" value="<?= $row['user_log_id']; ?>">
+                                                                                                    <button type="submit" name="delete_log" class="btn btn-sm btn-danger">Delete</button>
+                                                                                               </form>
+                                                                                          </td>
+                                                                                     </tr>
+                                                                      <?php
+                                                                                }
+                                                                           }
+                                                                      } else {
+                                                                           $result = mysqli_query($con, "SELECT * FROM user_log ORDER BY date_log DESC, time_log DESC");
+                                                                           while ($row = mysqli_fetch_array($result)) {
+                                                                      ?>
+                                                                                     <tr>
+                                                                                          <td><?= date("M d, Y", strtotime($row['date_log'])); ?></td>
+                                                                                          <td><?= date("h:i a", strtotime($row['time_log'])); ?></td>
+                                                                                          <td><?= $row['firstname'] . ' ' . $row['middlename'] . ' ' . $row['lastname']; ?></td>
+                                                                                          <td><?= $row['year_level'] . ' - ' . $row['course']; ?></td>
+                                                                                          <td><?= date("h:i a", strtotime($row['time_out'])); ?></td>
+                                                                                          <td>
+                                                                                               <form method="POST" action="" onsubmit="return confirm('Are you sure you want to delete this log?');">
+                                                                                                    <input type="text" name="delete_log_id" value="<?= $row['user_log_id']; ?>">
+                                                                                                    <button type="submit" name="delete_log" class="btn btn-sm btn-danger">Delete</button>
+                                                                                               </form>
+                                                                                          </td>
+                                                                                     </tr>
+                                                                      <?php
+                                                                           }
+                                                                      }
+                                                                      ?>
                                                                  </tbody>
                                                             </table>
                                                        </div>
@@ -142,10 +125,8 @@ include('./includes/sidebar.php');
                                    </div>
                               </div>
                          </div>
-
                     </div>
                </div>
-
           </div>
      </section>
 </main>
@@ -193,7 +174,7 @@ include('./includes/sidebar.php');
             language: {
                 buttons: {
                     copyTitle: 'Added to clipboard',
-                    copyKeys: 'Press <i>ctrl</i> or <i>\u2318</i> + <i>C</i> to copy the table data to your clipboard. <br><br>To cancel, click this message or press Esc.',
+                    copyKeys: 'Press <i>ctrl</i> or <i>⌘</i> + <i>C</i> to copy. Press Esc to cancel.',
                     copySuccess: {
                         _: '%d rows copied',
                         1: '1 row copied'
@@ -203,12 +184,10 @@ include('./includes/sidebar.php');
         });
     };
 
-    // Function to close the modal
     function closeModal() {
         document.getElementById('monthModal').style.display = 'none';
     }
 
-    // Function to filter by month and open new tab
     function filterByMonth() {
         const month = document.getElementById('selectedMonth').value;
         if (month) {
@@ -223,12 +202,8 @@ include('./includes/sidebar.php');
     initDataTable('#example3');
 </script>
 
-
-
 <?php 
 include('./includes/footer.php');
 include('./includes/script.php');
 include('../message.php');
-
-    
 ?>
