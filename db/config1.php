73145bde6a107c911cbed0b6442d82e6a3648bbd
Dark Coder <coder.dark2025@gmail.com>
2025-05-25 07:13:17 +0800

    v

--- db/config1.php
+++ db/config1.php
@@ -2,7 +2,7 @@
 <?php
 $servername = "127.0.0.1";
 $username = "u510162695_D10v1n";
-$password = "MCc_lRCv3rs!on";
+$password = "MCc_lRCv3rs!on2";
 $dbname = "u510162695_D10v1n_mcC_Lrc";
 // $servername = "127.0.0.1";
 // $username = "u510162695_mcclrc";
