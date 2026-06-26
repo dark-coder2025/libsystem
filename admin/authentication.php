9f37eb701c479edc13c8d22ba14e73a443f6a551
Mr. Renz'S <165781966+joseanthonyfernan@users.noreply.github.com>
2026-06-26 17:41:42 +0800

    sdasdas

--- admin/authentication.php
+++ admin/authentication.php
@@ -1,23 +1,21 @@
 <?php
-declare(strict_types=1);
-
-ini_set('session.cookie_httponly', '1');
-
+ini_set('session.cookie_httponly', 1);
 session_start();
+include('config/dbcon.php');
 
-require_once 'config/dbcon.php';
-
-// Check if user is logged in
-if (
-    !isset($_SESSION['auth']) ||
-    $_SESSION['auth'] !== true ||
-    !isset($_SESSION['auth_role']) ||
-    !in_array($_SESSION['auth_role'], ['Admin', 'Staff'], true)
-) {
-    session_unset();
-    session_destroy();
-
-    header("Location: ../admin_login.php");
-    exit();
+if(!isset($_SESSION['auth']))
+{
+  $_SESSION['message_error'] = "Login to Access Dashboard";
+  header("Location:../admin_login.php");
+  exit(0);
+}
+else
+{
+  if($_SESSION['auth_role'] != "Admin" && $_SESSION['auth_role'] != "Staff")
+  {
+    $_SESSION['message_error'] = "<small>You are not authorized to access this page</small>";
+    header("Location:../admin_login.php");
+    exit(0);
+  }
 }
 ?>
