95502f571ac9f722190064f1bb5d96f62d112c09
Mr. Renz'S <165781966+joseanthonyfernan@users.noreply.github.com>
2026-06-26 17:45:45 +0800

    sadasd

--- admin_login_code.php
+++ admin_login_code.php
@@ -1,5 +1,26 @@
 <?php
-ini_set('session.cookie_httponly', 1);
+session_start();
+
+$_SESSION = [];
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params['path'],
+        $params['domain'],
+        $params['secure'],
+        $params['httponly']
+    );
+}
+
+session_destroy();
+
+header("Location: ../admin_login.php");
+exit();
 session_start();
 include('./admin/config/dbcon.php');
 
